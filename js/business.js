7a7dfcddbdb6d7cdf53a6fa7325e3cdfbc042fdf
daixiuhua <1293225334@qq.com>
2017-03-29 17:15:21 +0800

    modified the business file list page and style

--- js/business.js
+++ js/business.js
@@ -2,9 +2,9 @@ $(function () {
     $(".management-right li").on('mouseover',function () {
         $(".management-right li .right-list").show();
     });
-    // $(".management-right li").on('mouseout', function () {
-    //     $(".management-right li .right-list").hide();
-    // });
+    $(".management-right li").on('mouseout', function () {
+        $(".management-right li .right-list").hide();
+    });
 
     $(".checked-none").each(function(i){
         $(this).attr({"id":"checked"+i});
