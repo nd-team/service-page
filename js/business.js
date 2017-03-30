a1229faf279ced20545832839fe59f1c37445226
daixiuhua <1293225334@qq.com>
2017-03-30 15:13:15 +0800

    modified the business meeting page and the style and jquery

--- js/business.js
+++ js/business.js
@@ -42,4 +42,28 @@ $(function () {
         $('.review').hide();
         $("#modalbg").hide()
     });
+
+    // 文件切换
+    (function($){
+        $.fn.myTab = function(options){//将对象作为参数传进去
+            var defult = {parent:"",index:null},
+                setting =$.extend(defult,options);//extend方法合并两个对象，如果两个对象有相同属性，则后面的那个对象的属性覆盖前面那个对象的属性。
+            $(this).find(".file-pic").on("click",function(){
+                var $this=$(this),
+                    $index =$this.index();
+                $this.addClass("current").siblings().removeClass("current");
+                $(setting.parent).children(".file-list").eq($index).show().siblings().hide();//切换标签对应的内容
+
+
+            })
+            //判断进入页面显示那个标签样式和内容
+            if(setting.index==null){
+                $(this).find('.file-pic').eq(0).click();
+            }else{
+                $(this).find('.file-pic').eq(options.index).click();
+            }
+        }
+    })(jQuery)
+
+    $(".file-name").myTab({parent:".file-order",index:0});
 });
